260456842214b247cb99a55e44d29ac96701ffd9
Olawale <olapetanwale@outlook.com>
2024-02-17 11:07:30 +0100

    Add logs

--- src/test/java/BasesClasses/TestBase.java
+++ src/test/java/BasesClasses/TestBase.java
@@ -1,8 +1,9 @@
 package BasesClasses;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,21 +14,27 @@ import java.time.Duration;
 
 public class TestBase {
     DesiredCapabilities caps = new DesiredCapabilities();
-    AndroidDriver driver;
+    public AndroidDriver driver;
+    private static Logger log = LogManager.getLogger(TestBase.class);
     @BeforeClass
     public void mainSetup() {
         caps.setCapability("platformName", "Android");
         caps.setCapability("automationName", "UiAutomator2");
         caps.setCapability("deviceName", "Samsung SM-A325F");
         caps.setCapability("udid", "RZ8T30532KZ");
-        caps.setCapability("app", System.getProperty("user.dir") + "/src/test/java/resource/ApiDemos-debug.apk");
+//        caps.setCapability("app", System.getProperty("user.dir") + "/src/test/java/resource/ApiDemos-debug.apk");
         caps.setCapability("platformVersion", "13.0");
+        caps.setCapability("appPackage", "io.appium.android.apis");
+        caps.setCapability("appActivity", "io.appium.android.apis.ApiDemos");
+//        caps.setCapability("noReset", false);
 
         try {
-            driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), caps);
+            driver = new AndroidDriver(new URL("http://127.0.0.2:4724/wd/hub"), caps);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        }catch (MalformedURLException e) {
-            e.printStackTrace();
+            log.info("The driver has been created");
+            log.info("Test is starting");
+        }catch (MalformedURLException | SessionNotCreatedException e) {
+            log.error("An error occurred creating the driver");
         }
     }
 
@@ -41,6 +48,7 @@ public class TestBase {
 
     @AfterClass
     public void tearDown() {
+        log.info("Test is ending");
         if(null != driver) {
             driver.quit();
         }
--- src/test/java/Tests/AppTest.java
+++ src/test/java/Tests/AppTest.java
@@ -1,12 +1,19 @@
 package Tests;
 
 import BasesClasses.TestBase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 public class AppTest extends TestBase {
 
+    private static Logger log = LogManager.getLogger(AppTest.class);
+
     @Test
     public void validateAppLaunch() {
+        log.info("Inside AppTest");
+        driver.findElement(By.xpath("//*[contain (@text, 'app')]"));
         sleep(20);
     }
 }
